d56d47328fd5d3a203a53dbb9c8a5e89c88868ae
jshaipuka <jelena.shaipuka@gmail.com>
2022-07-19 15:10:36 +0300

    Basic sample of Azure Queue Service

---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "aqstestsample"
+}
+
+variable "region" {
+  default = "westus2"
+}
+
+variable "azurerm_storage_account" {
+  default = "aqstestsamplesa"
+}
+
+variable "azurerm_storage_queue" {
+  default = "aqstestsampleaqs"
+}
